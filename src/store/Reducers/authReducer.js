12a1c32c2e9a78ed921d7515da480363dadc6e82
Douglas Kuo <douglas168@yahoo.com>
2024-04-27 09:58:15 +0800

    finish redux setup

---
+++ src/store/Reducers/authReducer.js
@@ -0,0 +1,34 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import api from "../../api/api";
+
+export const admin_login = createAsyncThunk(
+  "auth/admin_login",
+  async (info, { rejectWithValue, fulfillWithValue }) => {
+    console.log(info);
+    try {
+      // const { data } = await api.post("/admin-login", info, {
+      //   withCredentials: true,
+      // });
+      // localStorage.setItem("accessToken", data.token);
+      // console.log(data)
+      // return fulfillWithValue(data);
+    } catch (error) {
+      // console.log(error.response.data)
+      return rejectWithValue(error.response.data);
+    }
+  }
+);
+
+export const authReducer = createSlice({
+  name: "auth",
+  initialState: {
+    successMessage: null,
+    errorMessage: null,
+    loader: false,
+    userInfo: "",
+  },
+  reducers: {},
+  extraReducers: () => {},
+});
+
+export default authReducer.reducer;
