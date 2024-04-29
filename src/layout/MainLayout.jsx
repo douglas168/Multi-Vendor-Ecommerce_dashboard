a3c2c657e3490e24eb1f31c0fd80469495e3d8a5
Douglas Kuo <douglas168@yahoo.com>
2024-04-29 19:47:29 +0800

    finish sidebar navigation

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
+import Header from "../layout/Header";
+import Sidebar from "../layout/Sidebar";
 
 const MainLayout = () => {
   return (
-    <div>
-      <h1>
-        Main Layout <Outlet />
-      </h1>
+    <div className="bg-[#cdcae9] w-full min-h-screen">
+      <Header></Header>
+      <Sidebar></Sidebar>
+      <div className="ml-0 lg:ml-[260px] pt-[95px] transition-all">
+        <Outlet />
+      </div>
     </div>
   );
 };
